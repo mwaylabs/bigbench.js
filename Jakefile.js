8c1632c897bf1c0c6d5a0d14cc214b6eb166c103
Thomas Fankhauser <tommy@southdesign.de>
2013-02-28 20:19:30 +0100

    automating release strategy

--- Jakefile.js
+++ Jakefile.js
@@ -1,5 +1,12 @@
+task('default', function(){ jake.Task['test'].invoke(); });
+
 desc('Built the docs with docco');
 task('doc', [], function (params) {
   jake.exec("docco bin/* modules/*");
   console.log("Successfully built in docs folder")
+});
+
+desc('Runs the Tests');
+task('test', { async: true }, function () {
+  jake.exec("NODE_ENV=test mocha --colors", { printStdout: true });
 });
